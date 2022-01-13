e60f5084f5c67e29a2eae4f5afff63819d8d3aae
Srivatsan Talkad <stalkad01@gmail.com>
2022-01-13 12:08:08 -0500

    Add comments

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
+// Function will return the sum of a and b.
 function add(a, b) {
 	return a + b;
 }
 
+// Function will return the difference of b from a.
 function subtract(a, b) {
 	return a - b;
 }
 
+// Function will return the product of a and b.
 function multiply(a, b) {
 	return a * b;
 }
 
+// Function will return the quotient of a by b.
 function divide(a, b) {
 	return a / b;
 }
 
+// Function will call the corresponding function and return its return value depending on the operator that is passed as an argument.
 function operate(a, b, operator) {
 	if (operator == "+") return add(a, b);
 	if (operator == "-") return subtract(a, b);
 	if (operator == "*") return multiply(a, b);
 	if (operator == "/") return divide(a, b);
 }
+// Global vars for storing what the previous number was, the current number, and the operator, correspondingly.
 let prev = "";
 let curr = "";
 let opStore = "";
 
+// This function is responsible for setting the event listeners for all the buttons on the calculator.
 function setEventListeners() {
+	// Query select the relevant classes.
 	const calcScreen = document.querySelector('.calculator-screen')
 	const numButtons = document.querySelectorAll('.button-num');
 	const opButtons = document.querySelectorAll('.button-operator');
@@ -86,26 +94,36 @@ function setEventListeners() {
 	// Add event listener for the C button.
 	const cButton = document.querySelector('.button-c');
 	cButton.addEventListener('click', () => {
-		console.log('TEST'); // debugging
+		// console.log('TEST'); // debugging
 
 		let currLength = curr.length;
 		let prevLength = prev.length;
 		let opLength = opStore.length;
 
-		console.log(`currlength : ${currLength}, prevLength: ${prevLength}, opLength: ${opLength}. `); // debugging
+		// console.log(`currlength : ${currLength}, prevLength: ${prevLength}, opLength: ${opLength}. `); // debugging
 
+		// This condition checks for if the user has already inputted the first number, the operator, and the second number.
 		if (currLength > 0 && prevLength > 0 && opLength > 0) {
 			curr = curr.slice(0, -1);
-
+		
+		// This condition checks for whether the user has only inputted the first number and the operator, but not the second number.
 		} else if (prevLength > 0 & opLength > 0) {
 			opStore = opStore.slice(0, -1);
 			curr = prev;
+
+		// This condition checks for whether the user has only inputted the first number, and not the operator and second number.
 		} else if (prevLength > 0) {
 			prev = prev.slice(0, -1);
+
+		// Otherwise check if the user has just performed a calculation.
 		} else if (currLength > 0 && prevLength == 0) {
 			curr = curr.slice(0, -1);
 		}
+
+		// Actually modify the screen that the user ses when the button is pressed.
 		calcScreen.textContent = (calcScreen.textContent).slice(0, -1);
+
+		// If user clears the screen, then erase everything stored in the global vars.
 		if (calcScreen.textContent == "") {
 			prev = "";
 			curr = "";
